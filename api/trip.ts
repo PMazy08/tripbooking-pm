aa1e7981bdc3a57ad66560e232fe57b9354dc967
PMazy <96629729+PMazy08@users.noreply.github.com>
2024-02-12 21:46:26 +0700

    first commit

---
+++ api/trip.ts
@@ -0,0 +1,142 @@
+import express from "express";
+import mysql from "mysql";
+import { TripPostRequest } from "../model/trip_post_request";
+import { queryAsync } from "../dbconnect";
+
+export const conn = mysql.createPool({
+    connectionLimit: 10,
+    host: "202.28.34.197",
+    user: "tripbooking",
+    password: "tripbooking@csmsu",
+    database: "tripbooking",
+  });
+
+export const router = express.Router();
+
+
+// Query params
+router.get('/', (req, res) => {
+    if (req.query.id){
+        res.send("Get in trip.ts Query id " + req.query.id);
+    }else {
+        conn.query('select * from trip', (err, result, fields)=>{
+            if(err){
+                res.status(400).json(err);
+            }else {
+                res.json(result);
+            }
+          });
+    }
+});
+
+// Path params
+router.get('/:id', (req, res) => {
+    let id = +req.params.id;
+    conn.query('select * from trip where idx = ?', [id], (err, result, fields)=>{
+        if(err){
+            res.status(400).json(err);
+        }else {
+            res.json(result);
+        }
+      });
+    // res.send("Get in trip.ts " + req.params.id)
+});
+
+// Body String
+router.post("/", (req, res) => {
+    let trip: TripPostRequest = req.body;
+    let sql = "INSERT INTO `trip`(`name`, `country`, `destinationid`, `coverimage`, `detail`, `price`, `duration`) VALUES (?,?,?,?,?,?,?)";
+    sql = mysql.format(sql, [
+        trip.name,
+        trip.country,
+        trip.destinationid,
+        trip.coverimage,
+        trip.detail,
+        trip.price,
+        trip.duration,
+      ]);
+      conn.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(201).json({ affected_row: result.affectedRows, last_idx: result.insertId });
+      });
+});
+
+router.get("/search/fields", (req, res) => {
+    conn.query(
+      "select * from trip where (idx IS NULL OR idx = ?) OR (name IS NULL OR name like ?)",
+      [ req.query.id, "%" + req.query.name + "%"],
+      (err, result, fields) => {
+      if (err) throw err;
+        res.json(result);
+      }
+    );
+  });
+
+
+router.delete("/:id", (req, res) => {
+let id = +req.params.id;
+conn.query("delete from trip where idx = ?", [id], (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ affected_row: result.affectedRows });
+});
+});
+
+
+// router.put("/:id", (req, res) => {
+//     let id = +req.params.id;
+//     let trip: TripPostRequest = req.body;
+//     let sql =
+//       "update  `trip` set `name`=?, `country`=?, `destinationid`=?, `coverimage`=?, `detail`=?, `price`=?, `duration`=? where `idx`=?";
+//     sql = mysql.format(sql, [
+//       trip.name,
+//       trip.country,
+//       trip.destinationid,
+//       trip.coverimage,
+//       trip.detail,
+//       trip.price,
+//       trip.duration,
+//       id
+//     ]);
+//     conn.query(sql, (err, result) => {
+//       if (err) throw err;
+//       res
+//         .status(201)
+//         .json({ affected_row: result.affectedRows });
+//     });
+//   });
+
+
+  router.put("/:id", async (req, res) => {
+    let id = +req.params.id;
+    let trip: TripPostRequest = req.body;
+    let tripOriginal: TripPostRequest | undefined;
+  
+    let sql = mysql.format("select * from trip where idx = ?", [id]);
+  
+    let result = await queryAsync(sql);
+    const rawData = JSON.parse(JSON.stringify(result));
+    console.log(rawData);
+    tripOriginal = rawData[0] as TripPostRequest;
+    console.log(tripOriginal);
+  
+    let updateTrip = {...tripOriginal, ...trip};
+    console.log(trip);
+    console.log(updateTrip);
+  
+      sql =
+        "update  `trip` set `name`=?, `country`=?, `destinationid`=?, `coverimage`=?, `detail`=?, `price`=?, `duration`=? where `idx`=?";
+      sql = mysql.format(sql, [
+        updateTrip.name,
+        updateTrip.country,
+        updateTrip.destinationid,
+        updateTrip.coverimage,
+        updateTrip.detail,
+        updateTrip.price,
+        updateTrip.duration,
+        id,
+      ]);
+      conn.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(201).json({ affected_row: result.affectedRows });
+      });
+  });
---
+++ app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { router as index } from "./api/index";
+import { router as trip } from "./api/trip";
+import { router as upload } from "./api/upload";
+import bodyParser from "body-parser";
+
+// app = web api application
+export const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use("/", index);
+app.use("/trip", trip);
+app.use("/upload", upload);
+app.use("/uploads", express.static("uploads"));
+
+// app.use("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
